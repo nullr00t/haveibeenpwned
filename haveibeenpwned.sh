0a44dc1d0add70713fd6c00441fcf11ac2fcbdd7
Justin Ball <ball.justinr@gmail.com>
2016-05-07 19:55:38 -0400

    Added files via upload

---
+++ haveibeenpwned.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash 
+
+
+echo  "Please enter email address to check against https://haveibeenpwned.com"
+
+read choice
+response=$(curl --write-out %{http_code} --silent --output /dev/null "https://haveibeenpwned.com/api/breachedaccount/$choice")
+
+if [ "$response" = "404" ];then
+    echo "Congratulations! You are safe "
+
+elif [ "$response" = "400" ];then
+    echo "Oops. Something went wrong." 
+ 
+  
+else
+    temp=$(curl --silent --request GET "https://haveibeenpwned.com/api/breachedaccount/$choice")
+    echo  "You have been pwned in "$temp" breach "
+    echo  "Do you require further Details?(y/n)"
+    read ans
+    if [ "$ans" = "y" ] || [ "$ans" = "Y" ] ;then
+	curl --silent --request GET "https://haveibeenpwned.com/api/v2/breachedaccount/$choice"|python -mjson.tool > $choice.txt
+	cat $choice.txt|sed -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/[@#\$%^&*()=039"]//g' -e 's/<\/td>//g' -e 's/<em>//g' -e 's/<\/em>//g' -e 's/<a//g'  -e 's/<\/a>//g'  >  Breach_$choice.txt
+	cat Breach_$choice.txt
+	rm $choice.txt
+
+	echo "Output saved it into Breach_$choice.txt file"
+    fi
+
+    if [ "$ans" = "n" ] || [ "$ans" = "N" ] ;then 	
+	exit 1
+    fi
+	    
+fi
